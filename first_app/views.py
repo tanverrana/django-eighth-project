54b3060c9cadc59f790dc234cc3e14c64dd0504d
pro <pro@prodeMacBook-Pro-2.local>
2023-08-30 11:07:38 +0600

    show user login message

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import RegisterForm
-
+from django.contrib import messages
 # Create your views here.
 
 
@@ -8,6 +8,7 @@ def home(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+            messages.success(request, 'Account created successfully')
             form.save()
             print(form.cleaned_data)
     else:
